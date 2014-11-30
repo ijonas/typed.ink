e5071ac7c763d2753fc9d5bb92c167fd1ebc2b42
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2014-11-30 10:36:29 +0000

    added browserify and reactify into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,14 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    rename = require('gulp-rename'),
-    transform = require('vinyl-transform'),
-    uglify = require('gulp-uglify'),
     browserify = require('browserify'),
-    gutil = require('gulp-util');
- 
+    source = require("vinyl-source-stream"),
+    reactify = require('reactify');
 
-// gulp.task('default', function () {
-//   var watcher = gulp.watch('./src/coffee/**/*.coffee', ['coffee']);
-//   watcher.on('change', function(event) {
-//     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-//   });
-// });
-
-gulp.task('browserify', function () {
-  var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
-  });
-  
-  return gulp.src(['./src/client/*.js'])
-    .pipe(browserified)
-    .pipe(uglify())
-    .pipe(gulp.dest('./public/js'));
+gulp.task('browserify', function(){
+  var b = browserify();
+  b.transform(reactify); // use the reactify transform
+  b.add('./src/client/app.js');
+  return b.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./public/'));
 });
