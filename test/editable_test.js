4ae836177fb22894e225c456f2d6750d886b9364
Ijonas Kisselbach <ijona.kisselbach@gmail.com>
2014-11-30 19:18:47 +0000

    simple passing test.

--- test/editable_test.js
+++ test/editable_test.js
@@ -3,10 +3,5 @@ var test = require('tape');
 test('fibwibblers and xyrscawlers', function (t) {
     t.plan(2);
 
-    var x = someModule();
-    t.equal(x.foo(2), 22);
-
-    x.beep(function (err, res) {
-        t.equal(res, 'boop');
-    });
+   t.equal(5,5);
 })
